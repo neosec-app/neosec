7cecb2e68982e08ee28de2e8833a4842424fe684
chimmysaan <196896268+chimmysaan@users.noreply.github.com>
2025-12-11 20:09:45 +0600

    User DB updated after hierarchy

--- server/models/User.js
+++ server/models/User.js
@@ -3,68 +3,81 @@ const { sequelize } = require('../config/db');
 const bcrypt = require('bcryptjs');
 
 const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [6, 100] // Minimum 6 characters
-    }
-  },
-  isApproved: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-    allowNull: false
-  },
-  role: {
-    type: DataTypes.ENUM('user', 'admin'),
-    defaultValue: 'user',
-    allowNull: false
-  }
-}, {
-  tableName: 'users',
-  timestamps: true,
-  hooks: {
-    // Hash password before creating user
-    beforeCreate: async (user) => {
-      if (user.password) {
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-      }
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
     },
-    // Hash password before updating if it changed
-    beforeUpdate: async (user) => {
-      if (user.changed('password')) {
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-      }
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [6, 100] // Minimum 6 characters
+        }
+    },
+    isApproved: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM('user', 'admin'),
+        defaultValue: 'user',
+        allowNull: false
+    },
+    accountType: {
+        type: DataTypes.ENUM('user', 'leader', 'admin'),
+        defaultValue: 'user',
+        allowNull: false
+    },
+    subscriptionTier: {
+        type: DataTypes.ENUM('free', 'basic', 'pro', 'enterprise'),
+        defaultValue: 'free',
+        allowNull: false
+    },
+    isPaid: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
     }
-  }
+}, {
+    tableName: 'users',
+    timestamps: true,
+    hooks: {
+        // Hash password before creating user
+        beforeCreate: async (user) => {
+            if (user.password) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        },
+        // Hash password before updating if it changed
+        beforeUpdate: async (user) => {
+            if (user.changed('password')) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        }
+    }
 });
 
 // Instance method to compare password
 User.prototype.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+    return await bcrypt.compare(candidatePassword, this.password);
 };
 
 // Instance method to get user data without password
 User.prototype.toJSON = function() {
-  const values = Object.assign({}, this.get());
-  delete values.password;
-  return values;
+    const values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
 };
 
 module.exports = User;
-
